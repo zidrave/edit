cee02c45b14a8e43949ab5a7ec024829f696494b
Leonard Hecker <leonard@hecker.io>
2025-04-08 23:29:44 +0200

    Set up a proper Azure build pipeline

--- build.rs
+++ build.rs
@@ -1,12 +1,13 @@
 fn main() {
-    if std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default() == "windows"
-        && std::env::var("CARGO_CFG_TARGET_ENV").unwrap_or_default() == "msvc"
-    {
-        let path = std::path::absolute("src/edit.exe.manifest").unwrap();
-        let path = path.to_str().unwrap();
-        println!("cargo::rerun-if-changed=src/edit.exe.manifest");
-        println!("cargo::rustc-link-arg-bin=edit=/MANIFEST:EMBED");
-        println!("cargo::rustc-link-arg-bin=edit=/MANIFESTINPUT:{}", path);
-        println!("cargo::rustc-link-arg-bin=edit=/WX");
+    if std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default() == "windows" {
+        winres::WindowsResource::new()
+            .set_manifest_file("src/edit.exe.manifest")
+            .set("FileDescription", "Microsoft Edit")
+            .set(
+                "LegalCopyright",
+                "© Microsoft Corporation. All rights reserved.",
+            )
+            .compile()
+            .unwrap();
     }
 }
