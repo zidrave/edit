75db7c509fe07277ed003cd0870274f32d3d1b77
Leonard Hecker <leonard@hecker.io>
2025-05-11 13:34:49 +0200

    Improved benchmark structure

--- benches/lib.rs
+++ benches/lib.rs
@@ -1,11 +1,11 @@
-use criterion::{BenchmarkId, Criterion, Throughput, criterion_group, criterion_main};
+use std::mem;
+
+use criterion::{BenchmarkId, Criterion, Throughput, black_box, criterion_group, criterion_main};
 use edit::helpers::*;
+use edit::simd::MemsetSafe;
 use edit::{simd, ucd};
 
-fn bench(c: &mut Criterion) {
-    let mut buffer1 = [0u8; 2048];
-    let mut buffer4 = [0u32; 2048];
-
+fn bench_ucd(c: &mut Criterion) {
     let reference = concat!(
         "In the quiet twilight, dreams unfold, soft whispers of a story untold.\n",
         "月明かりが静かに照らし出し、夢を見る心の奥で詩が静かに囁かれる\n",
@@ -15,46 +15,60 @@ fn bench(c: &mut Criterion) {
     let buffer = reference.repeat(10);
     let bytes = buffer.as_bytes();
 
-    let mut group = c.benchmark_group("ucd::MeasurementConfig::goto_logical");
-    group.throughput(Throughput::Bytes(bytes.len() as u64));
-    group.bench_function("basic", |b| {
-        b.iter(|| ucd::MeasurementConfig::new(&bytes).goto_logical(Point::MAX))
-    });
-    group.bench_function("word_wrap", |b| {
-        b.iter(|| {
-            ucd::MeasurementConfig::new(&bytes).with_word_wrap_column(50).goto_logical(Point::MAX)
+    c.benchmark_group("ucd::MeasurementConfig::goto_logical")
+        .throughput(Throughput::Bytes(bytes.len() as u64))
+        .bench_function("basic", |b| {
+            b.iter(|| ucd::MeasurementConfig::new(&bytes).goto_logical(Point::MAX))
         })
-    });
-    group.finish();
-
-    let mut group = c.benchmark_group("simd::memchr2");
-    for &size in &[8usize, 40, 72, 1032] {
-        group.throughput(Throughput::Bytes(size as u64 + 1));
-        group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, &size| {
-            buffer1.fill(b'a');
-            buffer1[size] = b'\n';
-            b.iter(|| simd::memchr2(b'\n', b'\r', &buffer1, 0));
+        .bench_function("word_wrap", |b| {
+            b.iter(|| {
+                ucd::MeasurementConfig::new(black_box(&bytes))
+                    .with_word_wrap_column(50)
+                    .goto_logical(Point::MAX)
+            })
         });
-    }
-    group.finish();
+}
 
-    let mut group = c.benchmark_group("simd::memset::<u8>");
-    for &size in &[8usize, 40, 72, 1032] {
-        group.throughput(Throughput::Bytes(size as u64));
-        group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, &size| {
-            b.iter(|| simd::memset(&mut buffer1[..size], 0));
-        });
-    }
-    group.finish();
+fn bench_simd_memchr2(c: &mut Criterion) {
+    let mut group = c.benchmark_group("simd");
+    let mut buffer_u8 = [0u8; 2048];
 
-    let mut group = c.benchmark_group("simd::memset::<u32>");
-    for &size in &[8usize, 40, 72, 1032] {
-        group.throughput(Throughput::Bytes(size as u64));
-        group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, &size| {
-            b.iter(|| simd::memset(&mut buffer4[..size / 4], 0));
-        });
+    for &bytes in &[8usize, 32 + 8, 64 + 8, KIBI + 8] {
+        group.throughput(Throughput::Bytes(bytes as u64 + 1)).bench_with_input(
+            BenchmarkId::new("memchr2", bytes),
+            &bytes,
+            |b, &size| {
+                buffer_u8.fill(b'a');
+                buffer_u8[size] = b'\n';
+                b.iter(|| simd::memchr2(b'\n', b'\r', black_box(&buffer_u8), 0));
+            },
+        );
     }
-    group.finish();
+}
+
+fn bench_simd_memset<T: MemsetSafe + Copy + Default>(c: &mut Criterion) {
+    let mut group = c.benchmark_group("simd");
+    let name = format!("memset<{}>", std::any::type_name::<T>());
+    let size = mem::size_of::<T>();
+    let mut buf: Vec<T> = vec![Default::default(); 2048 / size];
+
+    for &bytes in &[8usize, 32 + 8, 64 + 8, KIBI + 8] {
+        group.throughput(Throughput::Bytes(bytes as u64)).bench_with_input(
+            BenchmarkId::new(&name, bytes),
+            &bytes,
+            |b, &bytes| {
+                let slice = unsafe { buf.get_unchecked_mut(..bytes / size) };
+                b.iter(|| simd::memset(black_box(slice), Default::default()));
+            },
+        );
+    }
+}
+
+fn bench(c: &mut Criterion) {
+    bench_ucd(c);
+    bench_simd_memchr2(c);
+    bench_simd_memset::<u8>(c);
+    bench_simd_memset::<u32>(c);
 }
 
 criterion_group!(benches, bench);
