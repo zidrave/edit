ffa98fefb59aead78b0dce568f3d375173248cb5
Keks137 <124277584+keks137@users.noreply.github.com>
2025-06-02 14:27:09 +0200

    Improved grammar for German LargeClipboardWarning (#384)

--- src/bin/edit/localization.rs
+++ src/bin/edit/localization.rs
@@ -661,7 +661,7 @@ const S_LANG_LUT: [[&str; LangId::Count as usize]; LocId::Count as usize] = [
     // LargeClipboardWarningLine2
     [
         /* en      */ "You copied {size} which may take a long time to share.",
-        /* de      */ "Sie haben {size} kopiert, das Weitergeben könnte lange dauern.",
+        /* de      */ "Sie haben {size} kopiert. Das Weitergeben könnte länger dauern.",
         /* es      */ "Copiaste {size}, lo que puede tardar en compartirse.",
         /* fr      */ "Vous avez copié {size}, ce qui peut être long à partager.",
         /* it      */ "Hai copiato {size}, potrebbe richiedere molto tempo per condividerlo.",
